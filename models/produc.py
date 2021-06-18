cae66b78531f9a0a6775ed2646d271e4b2f80538
monika_3 <ac19msanchez1@inslaferreria.cat>
2021-06-18 09:57:54 +0200

    test button

---
+++ models/produc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class productproduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def iterate_combo_prod(self):
+        for record in self:
+            if record.is_combo:
+                for product in record.combo_product_id:
+                    print(product, "/"*25)
